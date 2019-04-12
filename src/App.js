495db4b05fa65e01a873a5bd9e195dbd35a5761f
Cristiane Mayara <cris.mayara@gmail.com>
2019-04-12 00:11:44 -0300

    Adding card divider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import Post from './components/Post';
-import Header from './components/Header';
+import React, { Component } from "react";
+import Post from "./components/Post";
+import Header from "./components/Header";
 
 const styles = {
   containerStyle: {
     flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: '#EBEDF0',
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#EBEDF0"
   },
   postsStyle: {
-    margin: '0 auto',
-    maxWidth: '500px',
-  },
+    margin: "0 auto",
+    maxWidth: "500px"
+  }
 };
 
 class App extends Component {
@@ -22,36 +22,38 @@ class App extends Component {
     this.state = {
       posts: [
         {
-          id: '001',
-          author: 'Cristiane Mayara',
-          picture: 'https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg',
-          date: 'há 5 min',
+          id: "001",
+          author: "Cristiane Mayara",
+          picture:
+            "https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg",
+          date: "há 5 min",
           text:
-            'Caros amigos, a infinita diversidade da realidade única nos obriga à análise das condições epistemológicas e cognitivas exigidas. Por outro lado, a complexidade dos estudos efetuados cumpre um papel essencial na formulação da fundamentação metafísica das representações. Se estivesse vivo, Foucault diria que a inversão do modelo hybris-nêmesis exige a precisão e a definição do sistema de conhecimento geral. Ora, o novo modelo estruturalista aqui preconizado pressupõe a admissão da existência a priori da definição espinosista de substância.',
+            "Caros amigos, a infinita diversidade da realidade única nos obriga à análise das condições epistemológicas e cognitivas exigidas. Por outro lado, a complexidade dos estudos efetuados cumpre um papel essencial na formulação da fundamentação metafísica das representações. Se estivesse vivo, Foucault diria que a inversão do modelo hybris-nêmesis exige a precisão e a definição do sistema de conhecimento geral. Ora, o novo modelo estruturalista aqui preconizado pressupõe a admissão da existência a priori da definição espinosista de substância."
         },
         {
-          id: '002',
-          author: 'Cicrano',
-          picture: 'https://www.veon.ie/wp-content/uploads/2016/06/hans.jpg',
-          date: 'há 20 min',
+          id: "002",
+          author: "Cicrano",
+          picture: "https://www.veon.ie/wp-content/uploads/2016/06/hans.jpg",
+          date: "há 20 min",
           text:
-            'Se, para Sócrates, o homem não era mais que sua alma, podemos sustentar que a teoria das pulsões ainda não demonstrou convincentemente como vai participar na mudança das condições epistemológicas e cognitivas exigidas. Seguindo o fluxo da corrente analítica anglo-saxônica, a complexidade dos estudos efetuados obstaculiza a apreciação da importância da corrente inovadora da qual fazemos parte. Deste modo, acabei de refutar a tese segundo a qual a consolidação das estruturas psico-lógicas exige a precisão e a definição das condições de suas incógnitas. Ora, o acompanhamento das preferências de consumo é condição necessária e suficiente da hipótese de que existem infinitos objetos.',
+            "Se, para Sócrates, o homem não era mais que sua alma, podemos sustentar que a teoria das pulsões ainda não demonstrou convincentemente como vai participar na mudança das condições epistemológicas e cognitivas exigidas. Seguindo o fluxo da corrente analítica anglo-saxônica, a complexidade dos estudos efetuados obstaculiza a apreciação da importância da corrente inovadora da qual fazemos parte. Deste modo, acabei de refutar a tese segundo a qual a consolidação das estruturas psico-lógicas exige a precisão e a definição das condições de suas incógnitas. Ora, o acompanhamento das preferências de consumo é condição necessária e suficiente da hipótese de que existem infinitos objetos."
         },
         {
-          id: '003',
-          author: 'Fulano de Tal',
-          picture: 'https://img2.thejournal.ie/inline/2470754/original/?width=428&version=2470754',
-          date: 'há 30 min',
+          id: "003",
+          author: "Fulano de Tal",
+          picture:
+            "https://img2.thejournal.ie/inline/2470754/original/?width=428&version=2470754",
+          date: "há 30 min",
           text:
-            'Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a teoria do utilitarismo nos arrasta ao labirinto de sofismas obscuros dos elementos envolvidos de maneira conclusiva? Nada se pode dizer a respeito.',
-        },
-      ],
+            "Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a teoria do utilitarismo nos arrasta ao labirinto de sofismas obscuros dos elementos envolvidos de maneira conclusiva? Nada se pode dizer a respeito."
+        }
+      ]
     };
   }
 
   renderPosts() {
     const { posts } = this.state;
-    const { containerStyle, postsStyle } = styles;
+    const { postsStyle, containerStyle } = styles;
     return (
       <div style={containerStyle}>
         <Header />
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,49 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import PostHeader from './PostHeader';
+import React from "react";
+import PropTypes from "prop-types";
+import PostHeader from "./PostHeader";
 
 const styles = {
-  headerStyle: {
-    height: 43,
-    display: 'flex',
-    width: '100%',
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-  },
   containerStyle: {
     padding: 16,
-    display: 'flex',
-    borderRadius: '2px',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    backgroundColor: '#FFFFFF',
-    margin: '30px 0px 30px 0px',
-    boxShadow: '1px 1px 3px 1px #9E9E9E',
+    display: "flex",
+    borderRadius: "2px",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    backgroundColor: "#FFFFFF",
+    margin: "30px 0px 30px 0px",
+    boxShadow: "1px 1px 3px 1px #9E9E9E"
+  },
+  headerStyle: {
+    height: 43,
+    display: "flex",
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "flex-start"
+  },
+  dividerStyle: {
+    width: "100%",
+    margin: "14px 0px",
+    borderTop: "1px solid #fafafb"
   },
   textStyle: {
     fontSize: 14,
-    marginTop: 14,
-    color: 'black',
-  },
+    color: "black"
+  }
 };
 
 const Post = ({ post }) => {
-  const {
-    author, picture, date, text,
-  } = post;
-  const { textStyle, containerStyle } = styles;
+  const { author, picture, date, text } = post;
+  const { textStyle, dividerStyle, containerStyle } = styles;
 
   return (
     <div style={containerStyle}>
       <PostHeader author={author} picture={picture} date={date} />
+      <hr style={dividerStyle} />
       <div style={textStyle}>{text}</div>
     </div>
   );
 };
 
 Post.propTypes = {
-  post: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired
 };
 
 export default Post;
