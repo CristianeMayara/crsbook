1dfd2f42450ab943bb67374fd0d61d8c8917f13f
Cristiane Mayara <cris.mayara@gmail.com>
2019-04-11 22:59:32 -0300

    Setting post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,46 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import PostHeader from './PostHeader';
 
-class Post extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const styles = {
+  headerStyle: {
+    height: 43,
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  containerStyle: {
+    padding: 16,
+    display: 'flex',
+    borderRadius: '2px',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    backgroundColor: '#FFFFFF',
+    margin: '30px 0px 30px 0px',
+    boxShadow: '1px 1px 3px 1px #9E9E9E',
+  },
+  textStyle: {
+    fontSize: 14,
+    marginTop: 14,
+    color: 'black',
+  },
+};
 
-  render() {
-    const {
-      post: { author, picture, date },
-    } = this.props;
+const Post = ({ post }) => {
+  const {
+    author, picture, date, text,
+  } = post;
+  const { textStyle, containerStyle } = styles;
 
-    return (
-      <Fragment>
-        <PostHeader author={author} picture={picture} date={date} />
-      </Fragment>
-    );
-  }
-}
+  return (
+    <div style={containerStyle}>
+      <PostHeader author={author} picture={picture} date={date} />
+      <div style={textStyle}>{text}</div>
+    </div>
+  );
+};
 
 Post.propTypes = {
   post: PropTypes.object.isRequired,
